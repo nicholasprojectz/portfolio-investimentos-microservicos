eca9dbe35e2986275d5af30738b725d49d88023b
Nicholas Rodrigues <nicholas.diefenthaler@gmail.com>
2026-05-11 20:59:58 -0300

    Commit inicial do projeto

---
+++ user-portfolio-service/src/main/java/com/portfolio/user_portfolio_service/UserPortfolioServiceApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.user_portfolio_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPortfolioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPortfolioServiceApplication.class, args);
+	}
+
+}
---
+++ user-portfolio-service/src/test/java/com/portfolio/user_portfolio_service/UserPortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.user_portfolio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserPortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
