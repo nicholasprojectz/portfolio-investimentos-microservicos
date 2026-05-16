5284c4162c3f3f1a953d0bbf9548c370cb759869
nicholasprojectz <142259875+nicholasprojectz@users.noreply.github.com>
2026-05-16 19:03:52 +0000

    Ajustes finais no api-gateway, agora ele está atuando como intermediador de requisicoes.

--- api-gateway/src/main/java/com/portfolio/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/portfolio/api_gateway/ApiGatewayApplication.java
@@ -2,12 +2,24 @@ package com.portfolio.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
+    // IGNORANDO O YAML: Esta classe força o Spring a criar a rota na memória.
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service-java", r -> r.path("/auth/**")
+                        .uri("http://localhost:8081"))
+                .route("portfolio-service-java", r -> r.path("/api/portfolio/**")
+                        .uri("http://localhost:8082"))
+                .build();
+    }
 }
